fcde2e08acbc06cbf855f1b7465823577cbcf89c
Ozike <99070829+Ozike@users.noreply.github.com>
2022-11-19 20:02:01 +0700

    Add files via upload

---
+++ WPF_FITNESS_CENTER/Helper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_FITNESS_CENTER.Model;
+
+namespace WPF_FITNESS_CENTER
+{
+    class Helper
+    {
+        private static Entities loginEntities;
+        public static Entities GetContext()
+        {
+            if (loginEntities == null)
+            {
+                loginEntities = new Entities();
+            }
+            return loginEntities;
+        }
+        public string SearchUsers(string login, string password)
+        {
+            
+            var users = loginEntities.Participant.Where(x => x.Login == login).FirstOrDefault();
+            var users1 = loginEntities.Coach.Where(x => x.Login == login).FirstOrDefault();
+            var users2 = loginEntities.Moderator.Where(x => x.Login == login).FirstOrDefault();
+            var users3 = loginEntities.Organizator.Where(x => x.Login == login).FirstOrDefault();
+
+            if (users == null && users1 == null && users2 == null && users3 == null)
+            {
+                return "Такого пользователя нет.";
+            }
+            else
+            {
+                if (users == null && users2 == null && users1 == null)
+                {
+                    if (users3.Password == password)
+                    {
+                        return "Вы авторизовались, как организатор";
+                    }
+                    else
+                    {
+                        return "Неправильно введён пароль.";
+                    }
+                }
+                else if(users == null && users1 == null && users3 == null)
+                {
+                    if (users2.Password == password)
+                    {
+                        return "Вы авторизовались, как модератор";
+                    }
+                    else
+                    {
+                        return "Неправильно введён пароль.";
+                    }
+                }
+                else if(users == null && users2 == null && users3 == null)
+                {
+                    if (users1.Password == password)
+                    {
+                        return "Вы авторизовались, как тренер";
+                    }
+                    else
+                    {
+                        return "Неправильно введён пароль.";
+                    }
+                }
+                else if (users1 == null && users2 == null && users3 == null)
+                {
+                    if (users.Password == password)
+                    {
+                        return "Вы авторизовались, как участник";
+                    }
+                    else
+                    {
+                        return "Неправильно введён пароль.";
+                    }
+                }
+                else
+                {
+                    return "Что-то пошло не так";
+                }
+
+            }
+        }
+    }
+}
---
+++ WPF_FITNESS_CENTER/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_FITNESS_CENTER.Model;
+
+namespace WPF_FITNESS_CENTER
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtBoxLogin.Text;
+            string password = txtBoxPassword.Password;
+            Hash.Hash hash = new Hash.Hash();
+            Helper helper = new Helper();
+            Entities database = Helper.GetContext();
+            if (login == "")
+            {
+                if (password == "")
+                {
+                    MessageBox.Show("Введите логин и пароль");
+                }
+                else
+                {
+                    MessageBox.Show("Введите логин");
+                }
+            }
+            else if (password == "")
+            {
+                MessageBox.Show("Введите пароль");
+            }
+            else
+            {
+                password = hash.hashing(password);
+                string result = helper.SearchUsers(login, password);
+                MessageBox.Show(result);
+            }
+        }
+    }
+}
